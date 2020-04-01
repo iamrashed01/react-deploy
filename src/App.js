ed0b961a29f1edda66c9fbc56b0ffbb8b07589e4
rashed02 <irashad42@gmail.com>
2020-04-01 22:59:29 +0600

    published

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import React, {Fragment, useEffect, useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    const [state, setState] = useState([]);
-    useEffect(() => {
-        fetch('https://api.covid19api.com/summary')
-            .then(response => response.json())
-            .then(data => setState(data))
-    }, []);
-
     return (
         <div className="App">
             <header className="App-header">
@@ -17,15 +10,6 @@ function App() {
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
-                {state.Countries !== undefined ? <p>
-                        {
-                            Object.keys(state.Countries).splice(1, 10).map(item => {
-                                return (
-                                    <p>{state.Countries[item].Country} : NewConfirmed: {state.Countries[item].NewConfirmed}</p>
-                                )
-                            })
-                        }
-                </p> : ''}
                 <a
                     className="App-link"
                     href="https://reactjs.org"
