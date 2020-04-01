2222eff8a490338fbe7b2d84c0154658438c20ce
rashed02 <irashad42@gmail.com>
2020-04-01 22:58:30 +0600

    Updates

---
+++ precache-manifest.9eba426b9d99b5bb6d944618236a27b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7acd391964420d52a945b8bee46743",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "cdd0e1ecd1a42301c599",
+    "url": "/react-deploy/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "03d1e8d8af9f7f27e8c9",
+    "url": "/react-deploy/static/js/2.c78cb2ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.c78cb2ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd0e1ecd1a42301c599",
+    "url": "/react-deploy/static/js/main.215e2d52.chunk.js"
+  },
+  {
+    "revision": "e143d97d06dc6b82504a",
+    "url": "/react-deploy/static/js/runtime-main.aebb071e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-deploy/static/media/logo.ee7cd8ed.svg"
+  }
+]);
